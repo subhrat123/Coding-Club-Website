fcbf4149092cb3d50be227f7730aea3ef9843a89
omana <omanandswami2005@gmail.com>
2024-12-19 23:00:11 +0530

    Testing Frontend Branch!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-const Home = () => <h1>Welcome to the Coding Club</h1>;
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* login */}
+        <Route path="/login" element={<Login />} />
+        {/* register */}
+        <Route path="/register" element={<SignUp />} />
       </Routes>
     </Router>
   );
